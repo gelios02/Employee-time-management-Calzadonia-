6cda04245b4e604566abc7b56a60fd2625cfc882
Антон Власенко <AntonVlasenko2015@yandex.ru>
2023-05-28 19:11:52 +0700

    Ver 3.0

--- Calzadonia_1111111/Calzadonia_1111111/Table/Admin.cs
+++ Calzadonia_1111111/Calzadonia_1111111/Table/Admin.cs
@@ -9,7 +9,7 @@ namespace Calzadonia_1111111.Table
         public string Username { get; set; }
         public string Password { get; set; }
 
-        public string link = ("https://hackers-df577-default-rtdb.firebaseio.com/");
+        public string link = ("Your_firebase_Real_Database_Link");
     }
 
 }
